f67e7ab3371da675970c9cae120dece88bfb7d8e
kaiser7410 <kaiser7410@hotmail.com>
2024-03-13 11:09:54 -0500

    ejercicio logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eMarket-angular';
+  isDisabled = false;
+  fileName = 'LOGO.png'
 }
